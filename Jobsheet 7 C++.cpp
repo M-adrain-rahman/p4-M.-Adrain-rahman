e902648abb66bdb634c0b1c9b34536db3620b0a5
M-adrain-rahman <64573675+M-adrain-rahman@users.noreply.github.com>
2020-05-12 23:41:44 +0800

    Create Jobsheet 7 C++.cpp
    Membuat program mengisi nilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM terakhir setiap Mahasiswa menggunakan bahasa C++.

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main (){#
+cout<<"Nama : M.Adrain rahman\n";
+cout<<"Nim : F1B019084\n";
+cout<<"Kelompok: Kelompok 18\n\n";
+int nim [14];
+for (int i = 0; i<= 14; i++){
+cout<< "Masukkan nim ke-1 : " << i+1;
+cin>>nim[i];
+}
+}
