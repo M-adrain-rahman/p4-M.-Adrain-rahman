daca586e83701930addf974d1d00e838d3ba319a
M-adrain-rahman <64573675+M-adrain-rahman@users.noreply.github.com>
2020-05-12 23:39:11 +0800

    Create Jobsheet 7 C.c
    Membuat program mengisi nilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM terakhir setiap Mahasiswa menggunakan bahasa C.

---
+++ Jobsheet 7 C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+printf ("Nama : M.Adrain rahman\n");
+printf ("Nim : F1B019084\n ");
+printf ("Kelompok : 18\n\n");
+
+int nim [14], i;
+for (i = 0; i<= 14; i++){
+printf ("Masukkan Nim ke-%i :", i+1);
+scanf ("%i", &nim[i]);
+}
+}
