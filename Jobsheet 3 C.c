7b17f4c3a8d2e8b3545005d1a3fc4f9f59d12fc5
M-adrain-rahman <64573675+M-adrain-rahman@users.noreply.github.com>
2020-05-12 23:13:52 +0800

    Create Jobsheet 3 C.c
    Membuat program penjumlahan matriks dua dimensi dengan menggunakan NIM dua orang Mahasiswa menggunakan bahasa C.

---
+++ Jobsheet 3 C.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+main (){
+	int nim1 [2][2]; 
+	int nim2 [2][2]; 
+	int hasil [2][2];
+	int i, j;
+	printf("Nama= M.Adrain rahman\nNIM=4\nKelompok=18");
+   	printf("\nNama=Didik indirman\nNIM=8\nKelompok=18");
+	for (i = 0; i < 2; i++){
+		for (j = 0; j < 2; j++){
+			printf ("Masukkan nilai index %d, %d = ", i, j);
+			scanf ("%d", &nim1[i][j]);
+		}
+	}
+	printf ("\n");
+	for (i = 0; i < 2; i++){
+		for (j = 0; j < 2; j++){
+	printf ("Masukkan nilai index %d, %d = ", i, j);
+	scanf ("%d", &nim2[i][j]);
+		}
+	}
+	for (i = 0; i < 2; i++){
+		for (j = 0; j < 2;j++){
+			hasil [i][j]=nim1[i][j]+nim2[i][j];
+			printf ("%d ", hasil[i][j]);
+		}
+		printf ("\n");
+	}
+	getch();
+}
